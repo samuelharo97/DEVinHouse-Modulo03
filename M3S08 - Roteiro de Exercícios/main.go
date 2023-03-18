ef1db010f85a1fa1043f65bfaea2a7f92f28708f
Samuel Haro <samuel.hm@hotmail.com>
2023-03-17 23:49:54 -0300

    finished - ex03

--- M3S08 - Roteiro de Exercícios/main.go
+++ M3S08 - Roteiro de Exercícios/main.go
@@ -1,13 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Exercício 01 - Escreva um programa em Go que receba dois inteiros como entrada e mostra no console sua soma e sua diferença.
 func sumAndDiff(num1 int, num2 int) {
 
 	sum := num1 + num2
+	diff := (num1 - num2)
 
-	diff := (num1 - num2) * -1
+	if diff < 0 {
+		diff = diff * -1
+	}
 
 	fmt.Printf("Sum is %d and difference is %d", sum, diff)
 }
@@ -20,8 +25,42 @@ func stringLen(str string) {
 
 }
 
+// Exercício 03 - Escreva um programa em Go que recebe um integer como input e mostra no console todos os números primos até chegar integer do input.
+
+func isPrime(num int) bool {
+	if num <= 1 {
+		return false
+	}
+	if num == 2 {
+		return true
+	}
+	if num%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func printPrimeNum(num int) {
+
+	for i := 0; i < num; i++ {
+
+		res := isPrime(i)
+
+		if res {
+			fmt.Println(i, "é um número primo")
+		}
+
+	}
+
+}
+
 func main() {
 
-	stringLen("Samuka")
+	printPrimeNum(100)
 
 }
