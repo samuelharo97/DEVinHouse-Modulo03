49ad2cbf8588a010b0b9709e709cab221db697e2
Samuel Haro <samuel.hm@hotmail.com>
2023-03-23 21:56:35 -0300

    ex 02 - done

--- M3S09 - Roteiro de Exercícios/main.go
+++ M3S09 - Roteiro de Exercícios/main.go
@@ -20,10 +20,30 @@ func arraySum(arr []int) int {
 
 }
 
+// Ex 2 - Encontre o maior valor em um array de inteiros
+
+func highestNum(arr []int) int {
+
+	highest := 0
+
+	for _, v := range arr {
+
+		if highest < v {
+			highest = v
+		}
+
+	}
+
+	fmt.Println(highest)
+
+	return highest
+
+}
+
 func main() {
 
-	arr := []int{3, 5, 2}
+	arr := []int{10, 5, 20, 15, 30}
 
-	arraySum(arr)
+	highestNum(arr)
 
 }
